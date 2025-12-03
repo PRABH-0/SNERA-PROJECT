2a0c4ccdcedfe5de75c89dd9a555b5551854685a
Prabhjot Singh <ps7584153@gmail.com>
2025-12-03 17:51:12 +0530

    inititial commit

---
+++ BE/Snera_Core/Entities/UserEntities/UserSkill.cs
@@ -0,0 +1,10 @@
+﻿namespace Snera_Core.Entities.UserEntities
+{
+    public class UserSkill
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Skill_Name { get; set; }
+
+        public Guid UserId { get; set; }
+    }
+}
